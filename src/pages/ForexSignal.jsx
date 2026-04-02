aa8398163af2291228496354b8d8f4a97ca9d247
Kritikas8349 <kritikasingh8349@gmail.com>
2026-04-02 10:47:45 +0530

    signal page

--- src/pages/ForexSignal.jsx
+++ src/pages/ForexSignal.jsx
@@ -12,7 +12,7 @@ const ForexSignal = () => {
                 <div className="fs-content">
                     <h1>
                         Turn Market Moves into
-                        Profitable  <br />
+                        <span> Profitable  <br /> Trades</span> 
                     </h1>
 
                     <p>
